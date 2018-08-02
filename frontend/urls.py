300aebc3c509769018d8909376c8b035241e20c2
wasMitNetzen <irgend@wasmitnetzen.de>
2018-08-02 13:41:03 +0200

    Add Screenshots model
    * Add model
    * Add model to admin
    * Show all entries as list

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('about/', views.AboutPageView.as_view(), name='about'),
+    path('request/', views.RequestPageView.as_view(), name='request'),
+    path('list/', views.ListPageView.as_view(), name='list'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,21 @@
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Screenshot
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class RequestPageView(TemplateView):
+    template_name = 'request.html'
+
+
+class ListPageView(ListView):
+    model = Screenshot
+    template_name = 'list.html'
+
