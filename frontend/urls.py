1d625538024a1c2619fc83e12f0c0e75dba84e9e
wasMitNetzen <irgend@wasmitnetzen.de>
2018-08-07 16:48:49 +0200

    Removed unnecessary pages, styled list of screenshots

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('about/', views.AboutPageView.as_view(), name='about'),
-    path('screenshots/new/', views.create_screenshot, name='request'),
-    path('screenshots/list/', views.ListPageView.as_view(), name='list'),
-    path('screenshots/view/<int:pk>/', views.view_screenshot, name='view'),
+    path('', views.create_screenshot, name='request'),
+    path('list/', views.ListPageView.as_view(), name='list'),
+    path('view/<int:pk>/', views.view_screenshot, name='view'),
 ]
