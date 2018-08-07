4368de22419aa977805cdaf182d1a8a81101566b
wasMitNetzen <irgend@wasmitnetzen.de>
2018-08-07 20:28:55 +0200

    Introduce URL validation

--- frontend/forms.py
+++ frontend/forms.py
@@ -3,13 +3,15 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
-
 class CreateScreenshotForm(forms.Form):
-    url = forms.URLField()
+    url = forms.URLField(widget=forms.TextInput(attrs={'class': 'urlinput'}))
 
-    def clean_renewal_date(self):
-        url = self.cleaned_data['url']
+    def clean_url(self):
+        urldata = self.cleaned_data['url']
 
-        # TODO: check url (no relative links, contains http(s):// etc)
+        # check url (no relative links, contains http(s):// etc)
+        # starts with http:// or https://
+        if urldata[:7] != "http://" and urldata[:8] != "https://":
+            raise ValidationError(_('Invalid URL'))
         # Remember to always return the cleaned data.
-        return data
+        return urldata
